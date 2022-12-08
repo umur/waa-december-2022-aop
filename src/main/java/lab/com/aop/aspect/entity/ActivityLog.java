034f44c531ae82b8314f17d31d7e230987ef2f1f
sandesh56 <sandeshrauth644@gmail.com>
2022-12-07 18:08:48 -0600

    Feat: entity for ActivityLog defined

---
+++ src/main/java/lab/com/aop/aspect/entity/ActivityLog.java
@@ -0,0 +1,20 @@
+package lab.com.aop.aspect.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class ActivityLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private LocalDate date;
+    private String operation;
+    private long duration;
+}
